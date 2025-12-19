beae1830720c6ca70740c3cab39c935a9fbbe315
yuthila_banuka <banukadeseram12@gmail.com>
2025-12-19 21:58:30 +0530

    updated the homepage

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,9 @@
-import Header from "./Header.jsx"
-import Footer from "./Footer.jsx"
+import Header from "./Header";
 
 function App() {
-  return(
-    <>
-      <Header/>
-      <Footer/>
-    </>
+  return (
+      <Header /> 
   );
 }
 
-export default App
+export default App;
